e778da2e3954d2aa0d36deaf35177f0ab4c238a6
unknown <kou.official1221@gmail.com>
2025-11-11 14:49:34 +0900

    ボタンクリック後、名前の部分をKokikunに変更。

--- Form1.vb
+++ Form1.vb
@@ -2,9 +2,8 @@
     Private Sub Btn_ShowMsg_Click(sender As Object, e As EventArgs) Handles Btn_ShowMsg.Click
 
         Dim FirstName As String
-        FirstName = "Rakesh"
+        FirstName = "Kokikun"
         MessageBox.Show("Good Morning " & FirstName & " How are you?")
 
     End Sub
 End Class
-powershell
