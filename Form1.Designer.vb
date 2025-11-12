1e340cbcc804761d78b4f0d77f56554ef9fb97ad
unknown <kou.official1221@gmail.com>
2025-11-12 12:52:45 +0900

    iPhoneというボタンを作成。

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,23 +23,34 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Btn_ShowMsg = New Button()
+        iphone16 = New Button()
         SuspendLayout()
         ' 
         ' Btn_ShowMsg
         ' 
         Btn_ShowMsg.BackColor = Color.Fuchsia
-        Btn_ShowMsg.Location = New Point(0, -3)
+        Btn_ShowMsg.Location = New Point(264, 130)
         Btn_ShowMsg.Name = "Btn_ShowMsg"
         Btn_ShowMsg.Size = New Size(148, 74)
         Btn_ShowMsg.TabIndex = 0
         Btn_ShowMsg.Text = "Click Me"
         Btn_ShowMsg.UseVisualStyleBackColor = False
         ' 
+        ' iphone16
+        ' 
+        iphone16.Location = New Point(538, 108)
+        iphone16.Name = "iphone16"
+        iphone16.Size = New Size(74, 119)
+        iphone16.TabIndex = 1
+        iphone16.Text = "iPhone 16"
+        iphone16.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(iphone16)
         Controls.Add(Btn_ShowMsg)
         Name = "Form1"
         Text = "Form1"
@@ -47,5 +58,6 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Btn_ShowMsg As Button
+    Friend WithEvents iphone16 As Button
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,9 +1,31 @@
 ﻿Public Class Form1
     Private Sub Btn_ShowMsg_Click(sender As Object, e As EventArgs) Handles Btn_ShowMsg.Click
 
-        Dim FirstName As String
-        FirstName = "Kokikun"
-        MessageBox.Show("Good Morning " & FirstName & " How are you?")
+
+
 
     End Sub
+
+    Private Sub iPhone16_Click(sender As Object, e As EventArgs) Handles iphone16.Click
+
+        Dim StrBrand As String
+        Dim StrModel As String
+        Dim IntBattery As Integer
+        Dim DoubScreenSize As Double
+        Dim DecPrice As Decimal
+        Dim BoolAvailability
+
+        StrBrand = "Apple"
+        StrModel = "iPhone 16"
+        IntBattery = 4500
+        DoubScreenSize = 6.7
+        DecPrice = 799.99
+        BoolAvailability = True
+
+        MessageBox.Show(StrBrand & Environment.NewLine & StrModel & Environment.NewLine &
+                        IntBattery & Environment.NewLine & DoubScreenSize & Environment.NewLine &
+                        DecPrice & Environment.NewLine & BoolAvailability)
+
+    End Sub
+
 End Class
